51db012896a6b646a3cf3c7d6b8480f44ddf17eb
benareli <evan.wirasetya@gmail.com>
2022-03-20 07:49:23 +0800

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const AUTH_API = 'http://127.0.0.1:8080/api/auth/';
+const AUTH_API = 'http://192.53.112.254:8080/api/auth/';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
