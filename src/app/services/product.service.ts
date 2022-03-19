fc8d822afa0fba1bb60782f4813dc9d1eb38f697
benareli <evan.wirasetya@gmail.com>
2022-03-19 20:09:34 +0800

    127

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, map, of } from 'rxjs';
 import { Product } from '../models/product.model';
 
-const baseUrl = 'http://0.0.0.0:8080/api/products';
+const baseUrl = 'http://127.0.0.1:8080/api/products';
 
 @Injectable({
   providedIn: 'root'
