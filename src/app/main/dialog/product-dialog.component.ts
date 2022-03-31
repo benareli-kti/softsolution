a90d8e0ce0f1b9fdf12570afd38b3f858c4ce070
benareli-kti <benareli@keyklik.com>
2022-03-31 15:54:35 +0800

    drag drop

--- src/app/main/dialog/product-dialog.component.ts
+++ src/app/main/dialog/product-dialog.component.ts
@@ -110,6 +110,18 @@ export class ProductDialogComponent implements OnInit {
 
   //Dialog Data
   clickedRows = null;
+  
+  //Handling Image
+  activeColor: string = 'green';
+  baseColor: string = '#ccc';
+  overlayColor: string = 'rgba(255,255,255,0.5)';
+  
+  dragging: boolean = false;
+  loaded: boolean = false;
+  imageLoaded: boolean = false;
+  imageSrc: string = '';
+  
+ 
 
   constructor(
     public dialogRef: MatDialogRef<ProductDialogComponent>,
@@ -353,6 +365,32 @@ export class ProductDialogComponent implements OnInit {
         },
         error: (e) => console.error(e)
     });
-    
+  }
+  
+  //handling image
+  handleDragEnter() {this.dragging = true;}
+  handleDragLeave() {this.dragging = false;}
+  handleDrop(e) {
+    e.preventDefault();
+    this.dragging = false;
+    this.handleInputChange(e);
+  }
+  handleImageLoad() {this.imageLoaded = true;}
+  handleInputChange(e) {
+    var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    var pattern = /image-*/;
+    var reader = new FileReader();
+    if (!file.type.match(pattern)) {
+      alert('invalid format');
+      return;
+    }
+    this.loaded = false;
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+  _handleReaderLoaded(e) {
+    var reader = e.target;
+    this.imageSrc = reader.result;
+    this.loaded = true;
   }
 }
